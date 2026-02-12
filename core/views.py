4bcb6621e0baea09a71f53696d8a33ffeaedf8aa
dbernardos <121075844+dbernardos@users.noreply.github.com>
2026-02-12 17:54:06 -0300

    página de produtos funcionado

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Produto
 
 def index(request):
     context = {'curso': 'Desenvolvimento de Sistemas'}
@@ -13,4 +14,6 @@ def contato(request):
     return render(request, 'contato.html', context)
 
 def produtos(request):
-    return render(request, 'produtos.html')
+    produtos = Produto.objects.all()
+    context = {'prod': produtos}
+    return render(request, 'produtos.html', context)
