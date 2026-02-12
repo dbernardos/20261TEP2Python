210b3acd9366d1053fe3a02985e14d37237f9589
dbernardos <121075844+dbernardos@users.noreply.github.com>
2026-02-12 16:16:49 -0300

    pagina index funcionando

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {'curso': 'Desenvolvimento de Sistemas'}
+    return render(request, 'index.html', context)
